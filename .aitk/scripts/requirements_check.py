654d42831e93f8aa7325136434af9ef4955f4f64
xieofxie <xieofxie@126.com>
2025-09-24 11:15:28 +0800

    update QNN_LLM (#126)
    * update QNN_LLM
    
    * add onnxruntime
    
    ---------
    
    Co-authored-by: hualxie <hualxie@microsoft.com>

--- .aitk/scripts/requirements_check.py
+++ .aitk/scripts/requirements_check.py
@@ -12,6 +12,10 @@ def req_is_subset(req1, req2):
         print(f"Requirement '{req1[0].name}' is subset of '{req2[0].name}'")
     else:
         print(f"Requirement '{req1[0].name}' is not a subset of '{req2[0].name}'")
+        missing = req1[1] - req2[1]
+        print(f"Missing lines in '{req2[0].name}':")
+        for req in missing:
+            print(f"  {req}")
         raise SystemExit(1)
 
 
@@ -19,4 +23,9 @@ def requirements_check():
     requirements_folder = Path(__file__).parent.parent / "requirements"
     WCR_lines = get_lines_from_file(requirements_folder / "requirements-WCR.txt")
     WCR_INIT_lines = get_lines_from_file(requirements_folder / "requirements-WCR_INIT.txt")
+    QNN_LLM_lines = get_lines_from_file(requirements_folder / "requirements-QNN_LLM.txt")
     req_is_subset(WCR_INIT_lines, WCR_lines)
+    req_is_subset(QNN_LLM_lines, WCR_lines)
+
+if __name__ == "__main__":
+    requirements_check()
